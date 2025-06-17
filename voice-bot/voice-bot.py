10d843a7dda9b843feed50b95d66515a7173c78e
donald1218 <donald.shih2000@gmail.com>
2025-06-17 22:21:11 +0800

    feat: add chosen model

--- voice-bot/voice-bot.py
+++ voice-bot/voice-bot.py
@@ -1,6 +1,6 @@
 import os
 import asyncio
-import logging
+import sys
 from livekit import rtc
 from faster_whisper import WhisperModel
 import torch
@@ -44,27 +44,44 @@ dotenv.load_dotenv()
 
 async def entrypoint(ctx: JobContext):
     await ctx.connect()
-    google_api_key = os.getenv("GOOGLE_API_KEY")
+    _base_url = os.getenv("SPEACHES_URL")
+    if base_url is None:
+        base_url = "http://localhost:8000/v1"
+        
     session = AgentSession(
         vad=silero.VAD.load(),
         # any combination of STT, LLM, TTS, or realtime API can be used
-        stt=openai.STT(model="Systran/faster-distil-whisper-small.en", base_url="http://140.113.208.76:8000/v1", api_key="1234567890"),
-        tts=openai.TTS(model="speaches-ai/Kokoro-82M-v1.0-ONNX", voice="af_heart",speed=1.0, base_url="http://140.113.208.76:8000/v1", api_key="1234567890"),
+        stt=openai.STT(model="Systran/faster-distil-whisper-small.en", base_url=_base_url, api_key="1234567890"),
+        tts=openai.TTS(model="speaches-ai/Kokoro-82M-v1.0-ONNX", voice="af_heart",speed=1.0, base_url=_base_url, api_key="1234567890"),
     )
+    
+
+    
+    _google_api_key = os.getenv("GOOGLE_API_KEY")
+    _first_participant = list(ctx.room.remote_participants.values())[0]
+    _chosen_model = _first_participant.attributes["model"]
+    if _chosen_model is None:
+        _chosen_model = "gemma-3-27b-it"
+        
+    print("chosen model", _chosen_model)
 
     await session.start(
         room=ctx.room,
         agent=Agent(
             instructions="You are a friendly voice assistant built by LiveKit. Your output will be in the voice of the user. Formatted output or Emoji are not allowed.",
             llm=LLM(
-            model="gemma-3-27b-it",
-            api_key=google_api_key,
+            model=_chosen_model,
+            api_key=_google_api_key,
         )),
     )
     await session.generate_reply(instructions="greet the user and ask about their day after receiving '.' from the user")
     
 
 if __name__ == "__main__":
-    api_key = os.getenv("LIVEKIT_API_KEY")
-    api_secret = os.getenv("LIVEKIT_SECRET")
-    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint, api_key=api_key, api_secret=api_secret))
+    _api_key = os.getenv("LIVEKIT_API_KEY")
+    _api_secret = os.getenv("LIVEKIT_SECRET")
+    _google_api_key = os.getenv("GOOGLE_API_KEY")
+    if _google_api_key is None:
+        print("Google API key is required for Google API either via GOOGLE_API_KEY environment variable or api_key parameter")
+        sys.exit(0)
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint, api_key=_api_key, api_secret=_api_secret))
