bf4ae9e7a2da3e7a0c6aa2bb2e434c231e4864ff
donald1218 <donald.shih2000@gmail.com>
2025-06-17 22:25:27 +0800

    fix: wrong name with base_url and remove unused code

--- voice-bot/voice-bot.py
+++ voice-bot/voice-bot.py
@@ -1,56 +1,27 @@
 import os
-import asyncio
 import sys
-from livekit import rtc
-from faster_whisper import WhisperModel
-import torch
-import numpy as np
 import dotenv
 from livekit.agents import (
     Agent,
     AgentSession,
     JobContext,
-    RunContext,
     WorkerOptions,
     cli,
-    function_tool,
 )
-from livekit.plugins import openai, silero, google, langchain
-from livekit.rtc import Room
-from langgraph.graph import StateGraph
-from lang_graph import LLM, LLMStream
+from livekit.plugins import openai, silero
+from lang_graph import LLM
 
 dotenv.load_dotenv()
-
-# from dia.model import Dia
-
-# logging.basicConfig(level=logging.INFO)
-# logger = logging.getLogger("VoiceBot")
-
-# executor = ThreadPoolExecutor(max_workers=os.cpu_count() or 4)
-
-# asr_model = WhisperModel("small", device="cuda" if torch.cuda.is_available() else "cpu")
-
-# dia_agent = None 
-# try:
-#     dia_agent = Dia.from_pretrained("nari-labs/Dia-1.6B", compute_dtype="float16")
-#     logger.info("✨ Dia Agent initialized successfully.")
-# except Exception as e:
-#     logger.error(f"Failed to initialize Dia Agent: {e}. Please ensure the model is downloaded and compatible.")
-#     logger.info("Dia functionality will be skipped.")
-
-
 
 
 async def entrypoint(ctx: JobContext):
     await ctx.connect()
     _base_url = os.getenv("SPEACHES_URL")
-    if base_url is None:
-        base_url = "http://localhost:8000/v1"
+    if _base_url is None:
+        _base_url = "http://localhost:8000/v1"
         
     session = AgentSession(
         vad=silero.VAD.load(),
-        # any combination of STT, LLM, TTS, or realtime API can be used
         stt=openai.STT(model="Systran/faster-distil-whisper-small.en", base_url=_base_url, api_key="1234567890"),
         tts=openai.TTS(model="speaches-ai/Kokoro-82M-v1.0-ONNX", voice="af_heart",speed=1.0, base_url=_base_url, api_key="1234567890"),
     )
